578995012d534ec1d15027aeb7e2aa58d6a10d2e
Sara Ruiz <svruiz@uamv.edu.ni>
2026-06-04 10:48:18 -0600

    fix: reparar orden de creacion de tablas

--- practica_sql.sql
+++ practica_sql.sql
@@ -1,13 +1,14 @@
 use master
 go
-if exists(select 1 from sys.databases where name like 'HospitalDB')
+if exists(select 1 from sys.databases where name = 'HospitalDB')
 begin
 	drop database HospitalDB
 end
 go
 create database HospitalDB
+go
 --Mostrar todas las bases de datos existentes.
-select * from sys.databases
+--select * from sys.databases
 use HospitalDB
 go
 --Creacion de schemas
@@ -19,15 +20,13 @@ create schema Locacion
 go
 
 --crear tablas
---Paciente
-create table Personal.Pacientes(
-	id_paciente int identity(1,1) primary key,
-	nombre_paciente nvarchar(50) not null,
-	correo_paciente nvarchar(50),
-	edad_paciente int,
+--Habitaciones
+create table Locacion.Habitaciones(
+	id_habitacion int identity(1,1) primary key,
+	paciente nvarchar(50) not null,
 	created_at datetime default getdate(),
-	uptaded_at datetime default getdate(),
-	deleted_atcdate datetime default getdate()
+	updated_at datetime null,
+	deleted_at datetime null
 )
 go
 --Medicos
@@ -38,60 +37,78 @@ create table Personal.Medicos(
 	edad_medico int,
 	salario decimal (10,2) not null,
 	created_at datetime default getdate(),
-	updated_at datetime default getdate(),
-	deleted_at datetime default getdate()
+	updated_at datetime null,
+	deleted_at datetime null,
+	constraint uq_correo_medico unique (correo_medico),
+	constraint ck_edad_medico check (edad_medico>0),
+	constraint ck_salario check (salario>0)
+
 )
 go
+--Paciente
+create table Personal.Pacientes(
+	id_paciente int identity(1,1) primary key,
+	nombre_paciente nvarchar(50) not null,
+	correo_paciente nvarchar(50),
+	edad_paciente int,
+	habitacion int not null,
+	created_at datetime default getdate(),
+	uptaded_at datetime null,
+	deleted_at datetime null,
+	constraint uq_correo_paciente unique (correo_paciente),
+	constraint ck_edad_paciente check (edad_paciente>0),
+	constraint fk_habitacion foreign key (habitacion) references Locacion.Habitaciones (id_habitacion)
+)
+go
+
 --Especialidades 
 create table Personal.Especialidades(
-	id_especialidad int entity(1,1) primary key,
+	id_especialidad int identity(1,1) primary key,
 	nombre_especialidad nvarchar(50) not null,
+	id_medico int not null,
 	created_at datetime default getdate(),
-	updated_at datetime default getdate(),
-	deleted_at datetime default getdate()
+	updated_at datetime null,
+	deleted_at datetime null,
+	constraint fk_medico_especialidad foreign key (id_medico) references Personal.Medicos(id_medico)
 
 )
 go
 --Citas
 create table Monitoreo.Citas(
-	id_cita int entity(1,1) primary key,
-	paciente nvarchar(50) not null,
-	medico nvarchar(50) not null,
+	id_cita int identity(1,1) primary key,
+	id_paciente int not null,
+	id_medico int not null,
 	created_at datetime default getdate(),
-	updated_at datetime default getdate(),
-	deleted_at datetime default getdate()
-)
-go
---Habitaciones
-create table Locacion.Habitaciones(
-	id_habitacion int entity(1,1) primary key,
-	paciente nvarchar(50) not null,
-	created_at datetime default getdate(),
-	updated_at datetime default getdate(),
-	deleted_at datetime default getdate()
-)
-go
---Tratamiento
-create table Monitoreo.Tratamiento(
-	id_tratamiento int entity(1,1) primary key,
-	nombre_tratamiento nvarchar(50) not null,
-	paciente nvarchar(50),
-	medico nvarchar(50),
-	costo decimal(10,2) not null,
-	created_at datetime default getdate(),
-	updated_at datetime default getdate(),
-	deleted_at datetime default getdate()
-
+	updated_at datetime null,
+	deleted_at datetime null,
+	constraint fk_paciente foreign key (id_paciente) references Personal.Pacientes (id_paciente),
+	constraint fk_medico foreign key (id_medico) references Personal.Medicos (id_medico)
 )
 go
 --Medicamentos
 create table Monitoreo.Medicamento(
-	id_medicamento int entity(1,1) primary key,
+	id_medicamento int identity(1,1) primary key,
 	nombre_medicamento nvarchar(50) not null,
 	tratamiento nvarchar(50),
 	created_at datetime default getdate(),
-	updated_at datetime default getdate(),
-	deleted_at datetime default getdate()
+	updated_at datetime null,
+	deleted_at datetime null
 
+)
+go
+
+
+--Tratamiento
+create table Monitoreo.Tratamiento(
+	id_tratamiento int identity(1,1) primary key,
+	nombre_tratamiento nvarchar(50) not null,
+	id_paciente int,
+	costo decimal(10,2) not null,
+	medicamento int not null,
+	created_at datetime default getdate(),
+	updated_at datetime null,
+	deleted_at datetime null,
+	constraint fk_paciente_tratamiento foreign key (id_paciente) references Personal.Pacientes (id_paciente),
+	constraint fk_medicamento foreign key (medicamento) references Monitoreo.Medicamento (id_medicamento)
 )
 go
