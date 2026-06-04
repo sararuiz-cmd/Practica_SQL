bb70ca998a3beee42aaef8368dbdbb1bfe04465f
Sara Ruiz <svruiz@uamv.edu.ni>
2026-06-04 08:20:22 -0600

    feat: crear tabla pacientes

--- practica_sql.sql
+++ practica_sql.sql
@@ -18,3 +18,16 @@ create schema Monitoreo
 go
 create schema Locacion
 go
+
+--crear tablas
+--Paciente
+create table Personal.Pacientes(
+	id_paciente int identity(1,1) primary key,
+	nombre_paciente nvarchar(50) not null,
+	correo_paciente nvarchar(50),
+	edad_paciente int,
+	created_at datetime default getdate(),
+	uptaded_at datetime default getdate(),
+	deleted_atcdate datetime default getdate()
+)
+go
