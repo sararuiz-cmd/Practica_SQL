3eb378e3e77e1ee0b738baa0e7eb572adb987c32
Sara Ruiz <svruiz@uamv.edu.ni>
2026-06-09 07:59:16 -0600

    feat: crear tabla Departamento

--- Practica.sql
+++ Practica.sql
@@ -9,3 +9,14 @@ create database EmpresaSQL
 go
 use EmpresaSQL
 go
+create schema Ubicacion
+go
+create schema Empleados
+go
+create table Ubicacion.TDepartamento(
+	nDepartamentoID int identity(1,1) primary key,
+	cNombreDepartamento nvarchar(100) not null unique,
+	created_at datetime default getdate(),
+	updated_at datetime,
+	deleted_at datetime
+)go
