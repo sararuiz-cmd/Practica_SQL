992b7e385956a4248f9d0ded886cbf20dd283bf5
Sara Ruiz <svruiz@uamv.edu.ni>
2026-06-09 08:05:16 -0600

    feat: crear tabla Empleado

--- Practica.sql
+++ Practica.sql
@@ -27,3 +27,19 @@ create table Empleados.TCargo(
 	updated_at datetime,
 	deleted_at datetime
 )go
+create table Empleados.TEmpleado(
+	nEmpleadoID int identity(1,1) primary key,
+	cNIF varchar(8) not null unique,
+	cNombre nvarchar(100) not null,
+	cApellido nvarchar(100) not null,
+	nDepartamentoID int,
+	nCargoID int,
+	dFechaContratacion datetime not null default getdate(),
+	nSalario decimal(10,2) not null,
+	created_at datetime default getdate(),
+	updated_at datetime,
+	deleted_at datetime,
+	constraint ck_salario check(nSalario>300),
+
+
+)go
